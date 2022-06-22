4bdff514c5478b34ebb262da30fbf3043d852d2c
rafaapcode <rafaap2003@gmail.com>
2022-06-22 15:46:12 -0300

    Icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatIconModule } from '@angular/material/icon';
 import { UserListComponent } from './user/user-list.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   {
@@ -25,7 +27,8 @@ const routes: Routes = [
     FormsModule,
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
