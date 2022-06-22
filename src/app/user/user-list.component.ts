ad642150107b92390531fcab47d9eddcddada2ce
rafaapcode <rafaap2003@gmail.com>
2022-06-22 16:22:13 -0300

    Users added

--- src/app/user/user-list.component.ts
+++ src/app/user/user-list.component.ts
@@ -1,9 +1,59 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { User } from "./user";
+
+const USERS: User[] = [
+    {
+        imgUrl: "assets/imgs/person1.jpg",
+        name: "Rafael Silva",
+        email: "rafael@gmail.com",
+        site: "www.rafapcode.com",
+        date: new Date()
+    },
+
+    {
+        imgUrl: "assets/imgs/person2.jpeg",
+        name: "Flávia Albuquerque",
+        email: "flaalbuquerquer@hotmail.com",
+        site: "www.flaviacorretoras.com",
+        date: new Date()
+    },
+
+    {
+        imgUrl: "assets/imgs/person3.jpg",
+        name: "Marina Smith",
+        email: "marinaSM@gmail.com",
+        date: new Date()
+    },
+
+    {
+        imgUrl: "assets/imgs/person4.jpg",
+        name: "Barbara Caldeiras",
+        email: "barbaracaldeiras@yahoo.com",
+        date: new Date()
+    },
+
+    {
+        imgUrl: "assets/imgs/person5.jpg",
+        name: "Rafael Martins",
+        email: "rafaelMns@gmail.com",
+        site: "www.softwareDeveloperRafaelMartins.com.br",
+        date: new Date()
+    }
+]
+
+
+
 
 @Component({
     templateUrl: "./user-list.component.html",
     styleUrls: ["./user-list.component.css"]
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+
+    userss: User[] = [];
+
+    ngOnInit(): void {
+        this.userss = USERS;
+    }
 
 }
---
+++ src/app/user/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+    imgUrl: string;
+    name: string;
+    email: string;
+    site?: string;
+    date: Date;
+}
