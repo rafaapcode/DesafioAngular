15702e8894236df15cc760698255e2667e3d277d
rafaapcode <rafaap2003@gmail.com>
2022-06-22 15:24:14 -0300

    Table almost ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+import { UserListComponent } from './user/user-list.component';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {
+    path: "", redirectTo: "users", pathMatch: "full"
+  },
+  {
+    path: "users", component: UserListComponent
+  }
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/user-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "./user-list.component.html",
+    styleUrls: ["./user-list.component.css"]
+})
+export class UserListComponent {
+
+}
